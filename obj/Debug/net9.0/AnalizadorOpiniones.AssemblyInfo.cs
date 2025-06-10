59780d99aab3872b9d4759f6d330be50a61a1c1c
Smith <waltersmithchv@gmail.com>
2025-06-10 00:54:40 -0500

    Agregado

--- obj/Debug/net9.0/AnalizadorOpiniones.AssemblyInfo.cs
+++ obj/Debug/net9.0/AnalizadorOpiniones.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnalizadorOpiniones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f93522eaee725627319872675f8a8768551a87")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnalizadorOpiniones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnalizadorOpiniones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
